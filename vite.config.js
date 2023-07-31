80e2e6fb5447b186c1e7fe2eaf9d808412f2c36c
Lucas Eduardo Martins <93676786+LucasEduMartins@users.noreply.github.com>
2023-07-31 00:13:05 -0300

    remove unused module bundlers

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,24 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, "index.ts"),
       name: "MyLib",
       // the proper extensions will be added
-      fileName: "my-lib",
+      fileName: "index",
       formats: ["es"],
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue",
-        },
-      },
-    },
+    // rollupOptions: {
+    //   // make sure to externalize deps that shouldn't be bundled
+    //   // into your library
+    //   external: ["vue"],
+    //   output: {
+    //     // Provide global variables to use in the UMD build
+    //     // for externalized deps
+    //     globals: {
+    //       vue: "Vue",
+    //     },
+    //   },
+    // },
   },
   plugins: [dts()],
 });
